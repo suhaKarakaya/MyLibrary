d1be06a83c06c37b2c2ecc4033993a08727cef48
suhaKarakaya <suhabskarakaya@gmail.com>
2023-07-06 14:13:16 +0300

    add framework

---
+++ SuhaLibrary/SuhaLibrary/SuhaLibrary.h
@@ -0,0 +1,18 @@
+//
+//  SuhaLibrary.h
+//  SuhaLibrary
+//
+//  Created by Süha Karakaya on 6.07.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SuhaLibrary.
+FOUNDATION_EXPORT double SuhaLibraryVersionNumber;
+
+//! Project version string for SuhaLibrary.
+FOUNDATION_EXPORT const unsigned char SuhaLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SuhaLibrary/PublicHeader.h>
+
+
