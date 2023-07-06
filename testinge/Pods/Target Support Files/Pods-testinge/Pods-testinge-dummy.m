7e273c13d122eaab9bf1db44b940d7b07fa85d36
suhaKarakaya <suhabskarakaya@gmail.com>
2023-07-06 16:37:13 +0300

    last commit

--- testinge/Pods/Target Support Files/Pods-testinge/Pods-testinge-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_testinge : NSObject
-@end
-@implementation PodsDummy_Pods_testinge
-@end
--- testinge/Pods/Target Support Files/SuhaLibrary/SuhaLibrary-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SuhaLibrary : NSObject
-@end
-@implementation PodsDummy_SuhaLibrary
-@end
